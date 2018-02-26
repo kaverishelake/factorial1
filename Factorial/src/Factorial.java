4fd7c22eae5ef4c066c406ee9c5a266383112a57
MRUNAL <MRUNAL@DESKTOP-JTIIH6J>
2018-02-26 05:48:58 +0530

    second commit

--- Factorial/src/Factorial.java
+++ Factorial/src/Factorial.java
@@ -5,6 +5,7 @@ class Factorial{
   for(i=1;i<=number;i++){    
       fact=fact*i;    
   }    
-  System.out.println("Factorial of "+number+" is: "+fact);    
+  System.out.println("Factorial of "+number+" is: "+fact);   
+  System.out.println("code for second commit");
  }  
 }  
