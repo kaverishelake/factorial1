a2c01c11e4e3aff1d3e2f9e50b947ad800b14ed1
Sonali <kaverishelake>
2018-03-06 02:07:48 +0530

    last commit

--- Factorial/src/Factorial.java
+++ Factorial/src/Factorial.java
@@ -1,27 +1,11 @@
 class Factorial{  
  public static void main(String args[]){  
   int i,fact=1;  
-  int number=5;    
+  int number=4;    
   for(i=1;i<=number;i++){    
       fact=fact*i;    
   }    
   System.out.println("Factorial of "+number+" is: "+fact);   
-  System.out.println("code for second commit");
-  System.out.println("code for third commit");
-  System.out.println("code for fourth commit");
-   System.out.println("code for 5 commit");
-   System.out.println("code for 6th commit");
-   System.out.println("code for 7th commit");
-   System.out.println("code for 8th commit");
-
-     System.out.println("111");
-
-    // Sssssss1
-
-    //  Csjsjzvhsjsksks
-
-      System.out.println("code for 8th commit");
-     System.out.println("last commit from cmd prompt");
-      }  
+  }  
 
 }  
